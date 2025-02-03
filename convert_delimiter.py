222229a0adb93425435ae582e038c74dfa333a54
Liam Greene <wgreene@guilford.edu>
2025-02-03 12:50:02 -0500

    Adding SQLite and structuring database

--- convert_delimiter.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load CSV with tab delimiter
-df = pd.read_csv('energy-and-utilities-linc.csv', sep='\t')
-
-# Save with semicolon delimiter
-df.to_csv('energy-and-utilities-linc-converted.csv', sep=';', index=False)
-
